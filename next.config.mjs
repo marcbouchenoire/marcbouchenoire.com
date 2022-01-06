a622518a97599b0d460bb21398f04169609c9a0c
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-06 10:09:14 +0100

    Improve GitHub footer link

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
 import path from "path"
+import { execaSync } from "execa"
 import { writeJsonFileSync } from "write-json-file"
 
 const DATA_PATH = path.resolve("./src/data.json")
+
+/**
+ * Get the latest commit short hash.
+ */
+function getLatestCommit() {
+  const { stdout } = execaSync("git", ["rev-parse", "--short", "HEAD"])
+
+  return stdout
+}
 
 /**
  * Create a static data object.
  */
 function getData() {
   return {
-    date: String(new Date().getFullYear())
+    date: String(new Date().getFullYear()),
+    commit: getLatestCommit()
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
 export interface Data {
   /**
+   * The latest commit short hash.
+   */
+  commit: string
+
+  /**
    * The current year.
    */
   date: string
