eb6704fa251d798ecaa97b228e927d2c3c69e73d
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-18 23:47:53 +0100

    Revalidate latest film more often

--- src/hooks/use-latest-film.ts
+++ src/hooks/use-latest-film.ts
@@ -1,11 +1,11 @@
-import useSWRImmutable from "swr/immutable"
+import useSWR from "swr"
 import { Response } from "../pages/api/letterboxd/latest"
 
 /**
  * Fetch the latest film I watched from Letterboxd.
  */
 export function useLatestFilm(): Partial<Response> {
-  const { data } = useSWRImmutable<Response>("/api/letterboxd/latest")
+  const { data } = useSWR<Response>("/api/letterboxd/latest")
 
   return data ?? {}
 }
