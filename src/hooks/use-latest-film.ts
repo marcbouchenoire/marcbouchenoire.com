d86d8975b985c29e0eabc343b4e31a29da85a3c5
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-18 23:26:11 +0100

    Improve both API routes naming

--- src/hooks/use-latest-film.ts
+++ src/hooks/use-latest-film.ts
@@ -1,11 +1,11 @@
 import useSWRImmutable from "swr/immutable"
-import { Response } from "../pages/api/letterboxd"
+import { Response } from "../pages/api/letterboxd/latest"
 
 /**
  * Fetch the latest film I watched from Letterboxd.
  */
 export function useLatestFilm(): Partial<Response> {
-  const { data } = useSWRImmutable<Response>("/api/letterboxd")
+  const { data } = useSWRImmutable<Response>("/api/letterboxd/latest")
 
   return data ?? {}
 }
