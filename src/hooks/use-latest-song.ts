3bd098c0ec8a9d7b004ce6da18ce1d74f8d2ae21
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-18 23:26:11 +0100

    Improve both API routes naming

--- src/hooks/use-latest-song.ts
+++ src/hooks/use-latest-song.ts
@@ -1,11 +1,11 @@
 import useSWR from "swr"
-import { FRESH_DURATION, Response } from "../pages/api/lastfm"
+import { FRESH_DURATION, Response } from "../pages/api/lastfm/latest"
 
 /**
  * Fetch the latest song I listened to from Last.fm.
  */
 export function useLatestSong(): Partial<Response> {
-  const { data } = useSWR<Response>("/api/lastfm", {
+  const { data } = useSWR<Response>("/api/lastfm/latest", {
     refreshInterval: FRESH_DURATION * 1000
   })
 
