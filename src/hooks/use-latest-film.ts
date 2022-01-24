7c1ec6f203c255f4ad9abacd11c4b2eaca6b1d8e
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-25 00:41:11 +0100

    Lower the amount of data revalidations

--- src/hooks/use-latest-film.ts
+++ src/hooks/use-latest-film.ts
@@ -1,11 +1,13 @@
-import useSWR from "swr"
+import useSWR from "swr/immutable"
 import { Response } from "../pages/api/letterboxd/latest"
 
 /**
  * Fetch the latest film I watched from Letterboxd.
  */
 export function useLatestFilm(): Partial<Response> {
-  const { data } = useSWR<Response>("/api/letterboxd/latest")
+  const { data } = useSWR<Response>("/api/letterboxd/latest", {
+    revalidateIfStale: true
+  })
 
   return data ?? {}
 }
