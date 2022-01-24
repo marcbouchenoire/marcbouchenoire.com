d455aa9b97775c7bca6a7f4176ce8e2534b5269d
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-24 21:11:01 +0100

    Add Work section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Activity } from "../components/sections/Activity"
 import { Introduction } from "../components/sections/Introduction"
 import { Projects } from "../components/sections/Projects"
+import { Work } from "../components/sections/Work"
 
 /**
  * The index page component.
@@ -9,6 +10,7 @@ function Page() {
   return (
     <main className="flex-1 pb-12 md:pb-16 lg:pb-20">
       <Introduction className="mt-12 md:mt-16 lg:mt-20 content" />
+      <Work className="mt-16 md:mt-20 lg:mt-24" />
       <Projects className="mt-16 md:mt-20 lg:mt-24" />
       <Activity className="mt-16 md:mt-20 lg:mt-24 content" />
     </main>
