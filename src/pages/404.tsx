3b1bc6d63284b390c949bf2b66427b951234e16a
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-25 18:18:56 +0100

    Tweak button padding

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,7 +13,7 @@ function Page() {
         The page you are looking for doesn’t exist.
       </p>
       <Link href="/">
-        <a className="focusable flex w-full flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-primary-500 py-2 px-3 font-medium text-white shadow-lg shadow-primary-500/10 transition selection:bg-white/30 hover:bg-primary-500/80 hover:shadow-primary-500/5 dark:bg-primary-400 dark:text-zinc-900 dark:shadow-primary-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-primary-400/80 dark:hover:shadow-primary-400/5 sm:w-auto">
+        <a className="focusable flex w-full flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-primary-500 py-2 px-4 font-medium text-white shadow-lg shadow-primary-500/10 transition selection:bg-white/30 hover:bg-primary-500/80 hover:shadow-primary-500/5 dark:bg-primary-400 dark:text-zinc-900 dark:shadow-primary-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-primary-400/80 dark:hover:shadow-primary-400/5 sm:w-auto">
           Return to home page
         </a>
       </Link>
