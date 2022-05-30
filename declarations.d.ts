b09fbe65dbba3ca0ca114bb5a703826126ffe131
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-05-30 22:22:57 +0200

    Add SVG widgets as API routes

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,57 @@ declare module "*/data.json" {
 
   export default value
 }
+
+declare module "tailwindcss" {
+  export interface TailwindValues {
+    readonly [key: string]: string
+  }
+
+  export interface TailwindValuesColor {
+    readonly [key: string]: TailwindValues
+  }
+
+  export interface TailwindValuesFontFamily {
+    readonly [key: string]: string[]
+  }
+
+  export interface TailwindValuesFontSize {
+    readonly [key: string]: TailwindFontConfig
+  }
+
+  export interface TailwindTheme {
+    /**
+     * The theme's colors.
+     */
+    readonly colors: TailwindValuesColor
+
+    /**
+     * The theme's font families.
+     */
+    readonly fontFamily: TailwindValuesFontFamily
+
+    /**
+     * The theme's font sizes.
+     */
+    readonly fontSize: TailwindValuesFontSize
+
+    /**
+     * The theme's font weights.
+     */
+    readonly fontWeight: TailwindValues
+
+    /**
+     * The theme's letter spacings.
+     */
+    readonly letterSpacing: TailwindValues
+  }
+}
+
+declare module "tailwindcss/resolveConfig" {
+  export default function resolveConfig(config: any): {
+    /**
+     * The resolved Tailwind theme.
+     */
+    theme: import("tailwindcss").TailwindTheme
+  }
+}
