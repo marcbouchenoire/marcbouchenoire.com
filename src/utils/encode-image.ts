360bf1b1629b3bb4dc50e0e8e00a68659bfdc50c
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-06-01 22:38:08 +0200

    Resize and convert encoded images

--- src/utils/encode-image.ts
+++ src/utils/encode-image.ts
@@ -1,15 +1,16 @@
-import imageType from "image-type"
+import sharp, { ResizeOptions } from "sharp"
 
 /**
- * Fetch an image and encode it as a base64 data URI.
+ * Fetch an image, resize it and encode it as a JPEG base64 data URI.
  *
  * @param url - The image's URL.
+ * @param [options] - An optional set of resizing settings.
  */
-export async function encodeImage(url: string) {
-  const buffer = await fetch(url)
+export async function encodeImage(url: string, options: ResizeOptions = {}) {
+  const image = await fetch(url)
     .then((response) => response.arrayBuffer())
-    .then((arrayBuffer) => Buffer.from(arrayBuffer))
-  const type = imageType(buffer)
+    .then((arrayBuffer) => sharp(Buffer.from(arrayBuffer)))
+  const resized = await image.resize(options).jpeg().toBuffer()
 
-  return `data:${type?.mime};base64,${buffer.toString("base64")}`
+  return `data:image/jpeg;base64,${resized.toString("base64")}`
 }
