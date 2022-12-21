f67b081ef92a33f21e14135eafd4708185de2ac0
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-12-21 21:16:39 +0100

    Fix base64 encoding

--- src/utils/encode-image.ts
+++ src/utils/encode-image.ts
@@ -1,4 +1,4 @@
-import { toBase64 } from "fast-base64"
+import { toBase64 } from "fast-base64/js"
 
 /**
  * Fetch an image and encode it as a base64 data URI.
@@ -10,7 +10,7 @@ export async function encodeImage(url: string) {
   const buffer = await response.arrayBuffer()
   const array = new Uint8Array(buffer)
   const mime = response.headers.get("content-type") as string
-  const base64 = await toBase64(array)
+  const base64 = toBase64(array)
 
   return `data:${mime};base64,${base64}`
 }
