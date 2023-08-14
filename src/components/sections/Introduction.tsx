8ce081b9ea15f52ab00522ba6dfffb3adebbf955
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-08-14 09:24:58 +0200

    Tweak introduction and resume

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -40,7 +40,7 @@ export function Introduction(props: ComponentProps<"section">) {
         <p className="my-4 leading-loose">
           I’m currently living in Amsterdam and working at{" "}
           <a
-            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-orange-500 decoration-2 underline-offset-2 transition duration-100 hover:text-orange-500 hover:decoration-orange-500/30 focus:text-orange-500 focus:ring-orange-500/40 dark:text-white dark:decoration-orange-400 dark:hover:text-orange-400 dark:hover:decoration-orange-400/30 dark:focus:text-orange-400 dark:focus:ring-orange-400/40"
+            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-violet-500 decoration-2 underline-offset-2 transition duration-100 hover:text-violet-500 hover:decoration-violet-500/30 focus:text-violet-500 focus:ring-violet-500/40 dark:text-white dark:decoration-violet-400 dark:hover:text-violet-400 dark:hover:decoration-violet-400/30 dark:focus:text-violet-400 dark:focus:ring-violet-400/40"
             href="https://liveblocks.io/"
             rel="noreferrer"
             target="_blank"
@@ -67,7 +67,7 @@ export function Introduction(props: ComponentProps<"section">) {
           </a>{" "}
           and{" "}
           <a
-            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-lime-500 decoration-2 underline-offset-2 transition duration-100 hover:text-lime-500 hover:decoration-lime-500/30 focus:text-lime-500 focus:ring-lime-500/40 dark:text-white dark:decoration-lime-400 dark:hover:text-lime-400 dark:hover:decoration-lime-400/30 dark:focus:text-lime-400 dark:focus:ring-lime-400/40"
+            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-amber-500 decoration-2 underline-offset-2 transition duration-100 hover:text-amber-500 hover:decoration-amber-500/30 focus:text-amber-500 focus:ring-amber-500/40 dark:text-white dark:decoration-amber-400 dark:hover:text-amber-400 dark:hover:decoration-amber-400/30 dark:focus:text-amber-400 dark:focus:ring-amber-400/40"
             href="https://www.sketch.com/apps/"
             rel="noreferrer"
             target="_blank"
@@ -76,7 +76,7 @@ export function Introduction(props: ComponentProps<"section">) {
           </a>{" "}
           at{" "}
           <a
-            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-violet-500 decoration-2 underline-offset-2 transition duration-100 hover:text-violet-500 hover:decoration-violet-500/30 focus:text-violet-500 focus:ring-violet-500/40 dark:text-white dark:decoration-violet-400 dark:hover:text-violet-400 dark:hover:decoration-violet-400/30 dark:focus:text-violet-400 dark:focus:ring-violet-400/40"
+            className="focusable rounded-sm font-medium text-zinc-800 underline decoration-lime-500 decoration-2 underline-offset-2 transition duration-100 hover:text-lime-500 hover:decoration-lime-500/30 focus:text-lime-500 focus:ring-lime-500/40 dark:text-white dark:decoration-lime-400 dark:hover:text-lime-400 dark:hover:decoration-lime-400/30 dark:focus:text-lime-400 dark:focus:ring-lime-400/40"
             href="https://www.awkward.co/"
             rel="noreferrer"
             target="_blank"
@@ -119,27 +119,6 @@ export function Introduction(props: ComponentProps<"section">) {
           <span className="hidden md:inline">Twitter</span>
         </a>
         <a
-          className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-zinc-800 py-2 px-2.5 font-medium text-white shadow-lg shadow-zinc-800/10 transition selection:bg-white/30 hover:bg-zinc-800/80 hover:shadow-zinc-800/5 focus:ring-zinc-500/40 dark:bg-zinc-300 dark:text-zinc-900 dark:shadow-zinc-300/10 dark:selection:bg-zinc-900/30 dark:hover:bg-zinc-300/80 dark:hover:shadow-zinc-300/5 dark:focus:ring-zinc-300/40 md:w-auto md:px-3 md:pl-2.5"
-          href="https://github.com/marcbouchenoire"
-          rel="me noreferrer"
-          target="_blank"
-        >
-          <svg
-            height="24"
-            role="presentation"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clipRule="evenodd"
-              d="M12 2C6.475 2 2 6.47 2 11.988c0 4.42 2.862 8.153 6.838 9.476.5.087.687-.212.687-.474 0-.238-.013-1.024-.013-1.86C7 19.59 6.35 18.517 6.15 17.955c-.113-.287-.6-1.174-1.025-1.411-.35-.187-.85-.65-.013-.662.788-.012 1.35.724 1.538 1.024.9 1.51 2.338 1.086 2.912.824.088-.65.35-1.086.638-1.336-2.225-.25-4.55-1.111-4.55-4.931 0-1.087.387-1.986 1.025-2.685-.1-.25-.45-1.273.1-2.646 0 0 .837-.263 2.75 1.023a9.29 9.29 0 0 1 2.5-.337c.85 0 1.7.113 2.5.337 1.912-1.298 2.75-1.023 2.75-1.023.55 1.373.2 2.397.1 2.646.637.7 1.025 1.586 1.025 2.685 0 3.832-2.337 4.681-4.562 4.931.362.312.675.912.675 1.848 0 1.336-.013 2.41-.013 2.747 0 .262.188.574.688.474C19.137 20.141 22 16.395 22 11.988 22 6.47 17.525 2 12 2Z"
-              fill="currentColor"
-              fillRule="evenodd"
-            />
-          </svg>
-          <span className="hidden md:inline">GitHub</span>
-        </a>
-        <a
           className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-mastodon-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-mastodon-500/10 transition selection:bg-white/30 hover:bg-mastodon-500/80 hover:shadow-mastodon-500/5 focus:ring-mastodon-500/40 dark:bg-mastodon-400 dark:text-zinc-900 dark:shadow-mastodon-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-mastodon-400/80 dark:hover:shadow-mastodon-400/5 dark:focus:ring-mastodon-400/40 md:w-auto md:px-3 md:pl-2.5"
           href="https://mastodon.social/@marcbouchenoire"
           rel="me noreferrer"
@@ -159,6 +138,27 @@ export function Introduction(props: ComponentProps<"section">) {
             />
           </svg>
           <span className="hidden md:inline">Mastodon</span>
+        </a>
+        <a
+          className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-purple-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-purple-500/10 transition selection:bg-white/30 hover:bg-purple-500/80 hover:shadow-purple-500/5 focus:ring-purple-500/40 dark:bg-purple-400 dark:text-zinc-900 dark:shadow-purple-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-purple-400/80 dark:hover:shadow-purple-400/5 dark:focus:ring-purple-400/40 md:w-auto md:px-3 md:pl-2.5"
+          href="https://github.com/marcbouchenoire"
+          rel="me noreferrer"
+          target="_blank"
+        >
+          <svg
+            height="24"
+            role="presentation"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              d="M12 2C6.475 2 2 6.47 2 11.988c0 4.42 2.862 8.153 6.838 9.476.5.087.687-.212.687-.474 0-.238-.013-1.024-.013-1.86C7 19.59 6.35 18.517 6.15 17.955c-.113-.287-.6-1.174-1.025-1.411-.35-.187-.85-.65-.013-.662.788-.012 1.35.724 1.538 1.024.9 1.51 2.338 1.086 2.912.824.088-.65.35-1.086.638-1.336-2.225-.25-4.55-1.111-4.55-4.931 0-1.087.387-1.986 1.025-2.685-.1-.25-.45-1.273.1-2.646 0 0 .837-.263 2.75 1.023a9.29 9.29 0 0 1 2.5-.337c.85 0 1.7.113 2.5.337 1.912-1.298 2.75-1.023 2.75-1.023.55 1.373.2 2.397.1 2.646.637.7 1.025 1.586 1.025 2.685 0 3.832-2.337 4.681-4.562 4.931.362.312.675.912.675 1.848 0 1.336-.013 2.41-.013 2.747 0 .262.188.574.688.474C19.137 20.141 22 16.395 22 11.988 22 6.47 17.525 2 12 2Z"
+              fill="currentColor"
+              fillRule="evenodd"
+            />
+          </svg>
+          <span className="hidden md:inline">GitHub</span>
         </a>
         <a
           className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-dribbble-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-dribbble-500/10 transition selection:bg-white/30 hover:bg-dribbble-500/80 hover:shadow-dribbble-500/5 focus:ring-dribbble-500/40 dark:bg-dribbble-400 dark:text-zinc-900 dark:shadow-dribbble-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-dribbble-400/80 dark:hover:shadow-dribbble-400/5 dark:focus:ring-dribbble-400/40 md:w-auto md:px-3 md:pl-2.5"
