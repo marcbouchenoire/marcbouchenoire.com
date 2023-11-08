73f1c4fce3bf4e29f0db72b868cf32ed2b5597b0
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-11-08 22:57:30 +0100

    Use non-immutable version of SWR to load latest film

--- src/hooks/use-latest-film.ts
+++ src/hooks/use-latest-film.ts
@@ -1,4 +1,4 @@
-import useSWR from "swr/immutable"
+import useSWR from "swr"
 import type { Response } from "../app/api/letterboxd/latest/get-latest-film"
 import { json } from "src/utils/json"
 
@@ -6,9 +6,7 @@ import { json } from "src/utils/json"
  * Fetch the latest film I watched from Letterboxd.
  */
 export function useLatestFilm(): Partial<Response> {
-  const { data } = useSWR<Response>("/api/letterboxd/latest", json, {
-    revalidateIfStale: true
-  })
+  const { data } = useSWR<Response>("/api/letterboxd/latest", json)
 
   return data ?? {}
 }
