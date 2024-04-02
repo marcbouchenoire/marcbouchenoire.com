e1080d1e3a09b82df69bfd3a6b87648ccef7fe4b
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-04-02 11:09:14 +0200

    Add DevTools visual

--- src/app/sections/Work.tsx
+++ src/app/sections/Work.tsx
@@ -7,37 +7,64 @@ import elementsMusic from "public/visuals/elements-music.png"
 import motion from "public/visuals/framer-motion-secondary.png"
 import thesis from "public/visuals/thesis.png"
 
-interface BrowserProps extends ComponentProps<"div"> {
+interface BrowserProps extends Omit<ComponentProps<"div">, "title"> {
   /**
-   * The browser's current URL.
+   * Whether the window is a popup.
    */
-  url?: ReactNode
+  popup?: boolean
+
+  /**
+   * The window's current URL or title.
+   */
+  title?: ReactNode
+}
+
+interface PhoneProps extends ComponentProps<"div"> {
+  /**
+   * The phone's direction.
+   */
+  direction: "left" | "right"
 }
 
 /**
  * A Safari-like browser window.
  *
  * @param props - A set of `div` props.
- * @param [props.url] - The browser's current URL.
- * @param [props.children] - The browser's content.
+ * @param [props.popup] - Whether the window is a popup.
+ * @param [props.title] - The window's current URL or title.
+ * @param [props.children] - The window's content.
  * @param [props.className] - A list of one or more classes.
  */
-function Browser({ url, children, className, ...props }: BrowserProps) {
+function Browser({
+  title,
+  popup,
+  children,
+  className,
+  ...props
+}: BrowserProps) {
   return (
-    <div className={clsx(className, "browser")} {...props}>
+    <div
+      className={clsx(className, "browser", popup && "browser-popup")}
+      {...props}
+    >
       <div className="browser-header">
         <div className="flex gap-1.5">
           <div className="aspect-square w-2.5 rounded-full bg-black/10 dark:bg-white/20" />
           <div className="aspect-square w-2.5 rounded-full bg-black/10 dark:bg-white/20" />
           <div className="aspect-square w-2.5 rounded-full bg-black/10 dark:bg-white/20" />
         </div>
-        {url && (
-          <div className="flex h-5 select-none items-center justify-center rounded bg-black/5 px-3 text-3xs text-black/40 dark:bg-white/10 dark:text-white/40">
-            <span className="max-w-full truncate text-center leading-none">
-              {url}
-            </span>
-          </div>
-        )}
+        {title &&
+          (popup ? (
+            <div className="text-center text-3xs text-black/40 dark:text-white/40">
+              {title}
+            </div>
+          ) : (
+            <div className="flex h-5 select-none items-center justify-center rounded bg-black/5 px-3 text-3xs text-black/40 dark:bg-white/10 dark:text-white/40">
+              <span className="max-w-full truncate text-center leading-none">
+                {title}
+              </span>
+            </div>
+          ))}
       </div>
       <div className="flex-1">{children}</div>
     </div>
@@ -48,19 +75,27 @@ function Browser({ url, children, className, ...props }: BrowserProps) {
  * An iPhone-like device.
  *
  * @param props - A set of `div` props.
- * @param [props.children] - The browser's content.
+ * @param props.direction - The phone's direction.
+ * @param [props.children] - The phone's content.
  * @param [props.className] - A list of one or more classes.
  */
-function Phone({ children, className, ...props }: ComponentProps<"div">) {
+function Phone({ direction, children, className, ...props }: PhoneProps) {
   return (
-    <div className={clsx(className, "phone")} {...props}>
-      <div className="phone-screen-container">
+    <div
+      className={clsx(
+        className,
+        "phone dark:highlight-invert bg-white/80 shadow-floaty backdrop-blur-lg backdrop-saturate-200 before:absolute before:inset-0 before:rounded-[inherit] before:shadow-phone dark:bg-gray-800/80 dark:before:shadow-phone-invert",
+        `phone-${direction}`
+      )}
+      {...props}
+    >
+      <div className="phone-screen-container absolute drop-shadow-phone">
         <svg className="absolute" height={0} role="presentation" width={0}>
           <clipPath clipPathUnits="objectBoundingBox" id="phone-mask">
             <path d="M0.779,0.011 V0.011 c0,0.011,-0.019,0.026,-0.062,0.026 h-0.432 C0.241,0.037,0.222,0.022,0.222,0.011 v0 c0,-0.005,0,-0.011,-0.02,-0.011 H0.116 C0.043,0,0,0.02,0,0.053 v0.893 C0,0.98,0.043,1,0.116,1 h0.769 C0.957,1,1,0.98,1,0.947 V0.053 C1,0.02,0.957,0,0.884,0 h-0.085 C0.779,0,0.779,0.005,0.779,0.011" />
           </clipPath>
         </svg>
-        <div className="phone-screen">{children}</div>
+        <div className="phone-screen absolute inset-0">{children}</div>
       </div>
     </div>
   )
@@ -75,8 +110,14 @@ function Phone({ children, className, ...props }: ComponentProps<"div">) {
  */
 function Headset({ children, className, ...props }: ComponentProps<"div">) {
   return (
-    <div className={clsx(className, "headset")} {...props}>
-      <div className="headset-screen">
+    <div
+      className={clsx(
+        className,
+        "headset drop-shadow-floaty will-change-transform"
+      )}
+      {...props}
+    >
+      <div className="headset-screen absolute inset-0">
         <svg height={0} role="presentation" width={0}>
           <defs>
             <clipPath clipPathUnits="objectBoundingBox" id="headset-mask">
@@ -120,9 +161,11 @@ function Headset({ children, className, ...props }: ComponentProps<"div">) {
 function Book({ children, className, ...props }: ComponentProps<"div">) {
   return (
     <div className={clsx(className, "book")} {...props}>
-      <div className="book-back-cover" />
-      <div className="book-pages" />
-      <div className="book-cover">{children}</div>
+      <div className="book-back-cover absolute bg-current shadow-floaty" />
+      <div className="book-pages absolute z-20" />
+      <div className="book-cover absolute inset-0 z-20 overflow-hidden bg-current">
+        {children}
+      </div>
     </div>
   )
 }
@@ -144,7 +187,7 @@ export function Work(props: ComponentProps<"section">) {
         </p>
       </div>
       <div className="content lg:max-w-screen-md-12">
-        <div className="relative mb-5 flex flex-col rounded-lg bg-blue-500 shadow-xl shadow-blue-500/10 before:absolute before:inset-0 before:z-0 before:rounded-[inherit] before:bg-gradient-to-tl before:from-cyan-400/60 before:to-cyan-400/0 dark:bg-blue-400 dark:shadow-blue-400/10 dark:before:from-cyan-300/60 dark:before:to-cyan-300/0 sm:mb-8 sm:flex-row lg:mb-10">
+        <div className="relative z-10 mb-5 flex flex-col rounded-lg bg-blue-500 shadow-xl shadow-blue-500/10 before:absolute before:inset-0 before:z-0 before:rounded-[inherit] before:bg-gradient-to-tl before:from-cyan-400/60 before:to-cyan-400/0 dark:bg-blue-400 dark:shadow-blue-400/10 dark:before:from-cyan-300/60 dark:before:to-cyan-300/0 sm:mb-8 sm:flex-row lg:mb-10">
           <div className="z-10 flex-1 p-8 selection:bg-white/30 dark:selection:bg-blue-900/30 sm:basis-1/2 sm:p-12 sm:pr-10">
             <h3 className="mb-3 text-lg font-semibold leading-none text-white dark:text-blue-900">
               Liveblocks
@@ -157,7 +200,7 @@ export function Work(props: ComponentProps<"section">) {
               </span>
             </h3>
             <p className="leading-relaxed text-blue-100 dark:text-blue-900/80">
-              I slowly moved towards product engineering while at{" "}
+              At{" "}
               <a
                 className="focusable rounded-sm font-medium underline decoration-blue-100/30 decoration-2 underline-offset-2 transition duration-100 hover:decoration-blue-100/50 focus:ring-blue-100/30 dark:decoration-blue-900/20 dark:hover:decoration-blue-900/40 dark:focus:ring-blue-900/20"
                 href="https://liveblocks.io/"
@@ -166,38 +209,51 @@ export function Work(props: ComponentProps<"section">) {
               >
                 Liveblocks
               </a>
-              , starting with{" "}
+              , my focus slowly shifted towards the product. It started with{" "}
               <a
                 className="focusable rounded-sm font-medium underline decoration-blue-100/30 decoration-2 underline-offset-2 transition duration-100 hover:decoration-blue-100/50 focus:ring-blue-100/30 dark:decoration-blue-900/20 dark:hover:decoration-blue-900/40 dark:focus:ring-blue-900/20"
                 href="https://liveblocks.io/devtools"
                 rel="noreferrer"
                 target="_blank"
               >
-                a DevTools extension
+                our DevTools extension
               </a>{" "}
-              and then getting more and more involved in{" "}
+              and culminated with{" "}
               <a
                 className="focusable rounded-sm font-medium underline decoration-blue-100/30 decoration-2 underline-offset-2 transition duration-100 hover:decoration-blue-100/50 focus:ring-blue-100/30 dark:decoration-blue-900/20 dark:hover:decoration-blue-900/40 dark:focus:ring-blue-900/20"
-                href="https://liveblocks.io/docs"
+                href="https://liveblocks.io/comments"
                 rel="noreferrer"
                 target="_blank"
               >
-                the Liveblocks packages
+                Comments
               </a>
-              , focusing on{" "}
-              <a
-                className="focusable rounded-sm font-medium underline decoration-blue-100/30 decoration-2 underline-offset-2 transition duration-100 hover:decoration-blue-100/50 focus:ring-blue-100/30 dark:decoration-blue-900/20 dark:hover:decoration-blue-900/40 dark:focus:ring-blue-900/20"
-                href="https://react.dev/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                React
-              </a>
-              . From the components’ default styles to their APIs, from the
-              hooks to systems like batching and caching, and&nbsp;more.
+              . I worked on defining the React side of our focus on opinionated
+              products, from component APIs to default styles, from hooks to
+              unstyled primitives, and more.
             </p>
           </div>
-          <div className="relative flex flex-1 items-center justify-center p-12 pb-16 pt-0 sm:-mr-12 sm:basis-1/3 sm:p-0 lg:-mr-12 lg:basis-1/2" />
+          <div className="relative flex flex-1 items-center justify-center p-12 pb-16 pt-0 sm:-mr-12 sm:basis-1/3 sm:p-0 lg:-mr-12 lg:basis-1/2">
+            <div className="perspective pointer-events-none absolute inset-0">
+              <Browser
+                className="transform-liveblocks-devtools pointer-events-auto absolute bottom-[-23%] right-0 w-[50%]"
+                popup
+                title="DevTools"
+              >
+                <div className="relative aspect-[1460/1880] bg-black">
+                  <video
+                    autoPlay
+                    className="absolute h-full w-full object-cover"
+                    loop
+                    muted
+                    playsInline
+                    poster="/visuals/devtools.jpg"
+                    preload="metadata"
+                    src="/visuals/devtools.mp4"
+                  />
+                </div>
+              </Browser>
+            </div>
+          </div>
         </div>
         <div className="relative my-5 flex flex-col rounded-lg bg-violet-500 shadow-xl shadow-violet-500/10 before:absolute before:inset-0 before:z-0 before:rounded-[inherit] before:bg-gradient-to-tl before:from-fuchsia-400/60 before:to-fuchsia-400/0 dark:bg-violet-400 dark:shadow-violet-400/10 dark:before:from-fuchsia-300/60 dark:before:to-fuchsia-300/0 sm:my-8 sm:flex-row-reverse lg:my-10">
           <div className="z-10 flex-1 p-8 selection:bg-white/30 dark:selection:bg-violet-900/30 sm:basis-1/2 sm:p-12">
@@ -212,7 +268,7 @@ export function Work(props: ComponentProps<"section">) {
               </span>
             </h3>
             <p className="leading-relaxed text-violet-100 dark:text-violet-900/80">
-              Before working on{" "}
+              Before focusing on{" "}
               <a
                 className="focusable rounded-sm font-medium underline decoration-violet-100/30 decoration-2 underline-offset-2 transition duration-100 hover:decoration-violet-100/50 focus:ring-violet-100/30 dark:decoration-violet-900/20 dark:hover:decoration-violet-900/40 dark:focus:ring-violet-900/20"
                 href="https://liveblocks.io/docs"
@@ -283,7 +339,7 @@ export function Work(props: ComponentProps<"section">) {
               <div className="perspective pointer-events-none absolute inset-0">
                 <Browser
                   className="transform-liveblocks-io-secondary pointer-events-auto absolute bottom-0 left-0 w-[75%] lg:-bottom-8"
-                  url="liveblocks.io"
+                  title="liveblocks.io"
                 >
                   <div className="relative aspect-[920/560] bg-black">
                     <video
@@ -302,7 +358,7 @@ export function Work(props: ComponentProps<"section">) {
               <div className="perspective pointer-events-none absolute inset-0">
                 <Browser
                   className="transform-liveblocks-io-primary pointer-events-auto absolute right-0 top-0 w-[75%] lg:-top-3"
-                  url="liveblocks.io"
+                  title="liveblocks.io"
                 >
                   <div className="relative aspect-[920/560] bg-black">
                     <video
@@ -368,7 +424,10 @@ export function Work(props: ComponentProps<"section">) {
           </div>
           <div className="relative flex flex-1 items-center justify-center p-12 pb-16 pt-0 sm:-mr-12 sm:basis-1/3 sm:p-0 lg:-mr-12 lg:basis-1/2">
             <div className="perspective z-20 flex w-full max-w-sm items-center sm:absolute sm:inset-0 sm:max-w-none lg:-bottom-20">
-              <Browser className="transform-framer-com w-full" url="framer.com">
+              <Browser
+                className="transform-framer-com w-full"
+                title="framer.com"
+              >
                 <div className="relative aspect-[920/560] bg-black">
                   <video
                     autoPlay
@@ -414,7 +473,7 @@ export function Work(props: ComponentProps<"section">) {
               <div className="perspective pointer-events-none absolute inset-0">
                 <Browser
                   className="transform-motion-secondary pointer-events-auto absolute left-0 top-0 w-[60%] lg:-top-10 lg:w-[80%]"
-                  url={
+                  title={
                     <>
                       framer.com
                       <span className="text-black/20 dark:text-white/20">
@@ -438,7 +497,7 @@ export function Work(props: ComponentProps<"section">) {
               <div className="perspective pointer-events-none absolute inset-0">
                 <Browser
                   className="transform-motion-primary pointer-events-auto absolute bottom-0 right-0 w-[80%] lg:-bottom-2"
-                  url={
+                  title={
                     <>
                       framer.com
                       <span className="text-black/20 dark:text-white/20">
@@ -489,7 +548,10 @@ export function Work(props: ComponentProps<"section">) {
           </div>
           <div className="perspective relative z-20 flex flex-1 items-center justify-center sm:-mr-12 sm:basis-1/3">
             <div className="transform-elements mx-12 mb-24 mt-0 grid aspect-square w-full max-w-sm grid-cols-2 gap-8 sm:absolute sm:m-0 sm:max-w-[22rem] md:-top-16 lg:top-[-5.5rem] lg:grid-cols-[1fr_1.2fr]">
-              <Phone className="-mb-8 self-end md:-mb-16">
+              <Phone
+                className="relative -mb-8 self-end md:-mb-16"
+                direction="right"
+              >
                 <Image
                   alt="A food app interface"
                   className="elements absolute h-full w-full object-cover"
@@ -498,7 +560,7 @@ export function Work(props: ComponentProps<"section">) {
                   width="160"
                 />
               </Phone>
-              <Phone>
+              <Phone className="relative" direction="right">
                 <Image
                   alt="A music app interface"
                   className="elements elements-offset absolute h-full w-full object-cover"
@@ -531,8 +593,8 @@ export function Work(props: ComponentProps<"section">) {
             </p>
           </div>
           <div className="relative flex flex-1 items-center justify-center p-12 pt-0 sm:-ml-12 sm:aspect-auto sm:basis-1/3 sm:p-0 lg:basis-1/2">
-            <div className="relative z-20 aspect-[4/3] w-full max-w-sm sm:absolute sm:max-w-none lg:aspect-[9/7]">
-              <Headset className="w-[80%] rotate-[-6deg]">
+            <div className="perspective pointer-events-none relative z-20 aspect-[4/3] w-full max-w-sm bg-green-600/0 sm:absolute sm:max-w-none lg:aspect-[9/7]">
+              <Headset className="transform-degree-headset pointer-events-auto absolute top-[16%] w-[90%]">
                 <video
                   autoPlay
                   className="absolute h-full w-full object-cover"
@@ -544,7 +606,10 @@ export function Work(props: ComponentProps<"section">) {
                   src="/visuals/degree-headset.mp4"
                 />
               </Headset>
-              <Phone className="w-[30%] rotate-[6deg]">
+              <Phone
+                className="transform-degree-phone pointer-events-auto absolute right-[20%] top-[56%] w-[12%]"
+                direction="left"
+              >
                 <video
                   autoPlay
                   className="absolute h-full w-full object-cover"
@@ -556,7 +621,7 @@ export function Work(props: ComponentProps<"section">) {
                   src="/visuals/degree-phone.mp4"
                 />
               </Phone>
-              <Book className="w-[30%] rotate-[-6deg] text-thesis">
+              <Book className="transform-degree-thesis pointer-events-auto absolute right-0 top-[40%] w-[24%] text-thesis">
                 <Image
                   alt="The cover of my master’s thesis"
                   className="absolute h-full w-full"
