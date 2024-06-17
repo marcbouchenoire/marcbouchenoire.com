68b8c02ac5e338aed4e679af804f3f8f287177f1
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-17 23:34:06 +0200

    Use redirects instead

--- next.config.mjs
+++ next.config.mjs
@@ -44,61 +44,62 @@ export default () => {
     eslint: {
       ignoreDuringBuilds: true
     },
+    async redirects() {
+      return [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "symbolist.marcbouchenoire.com"
+            }
+          ],
+          destination: "/projects/symbolist/:path*",
+          permanent: true
+        },
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "typometer.marcbouchenoire.com"
+            }
+          ],
+          destination: "/projects/typometer/:path*",
+          permanent: true
+        },
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "dimmmensions.marcbouchenoire.com"
+            }
+          ],
+          destination: "/projects/dimmmensions/:path*",
+          permanent: true
+        }
+      ]
+    },
     async rewrites() {
-      return {
-        beforeFiles: [
-          {
-            source: "/:path*",
-            has: [
-              {
-                type: "host",
-                value: "symbolist.marcbouchenoire.com"
-              }
-            ],
-            destination: "https://marcbouchenoire-symbolist.vercel.app/:path*"
-          },
-          {
-            source: "/:path*",
-            has: [
-              {
-                type: "host",
-                value: "typometer.marcbouchenoire.com"
-              }
-            ],
-            destination: "https://marcbouchenoire-typometer.vercel.app/:path*"
-          },
-          {
-            source: "/:path*",
-            has: [
-              {
-                type: "host",
-                value: "dimmmensions.marcbouchenoire.com"
-              }
-            ],
-            destination:
-              "https://marcbouchenoire-dimmmensions.vercel.app/:path*"
-          }
-        ],
-        afterFiles: [
-          {
-            source: "/:path*",
-            destination: `/:path*`
-          },
-          {
-            source: "/projects/symbolist/:path*",
-            destination: "https://marcbouchenoire-symbolist.vercel.app/:path*"
-          },
-          {
-            source: "/projects/typometer/:path*",
-            destination: "https://marcbouchenoire-typometer.vercel.app/:path*"
-          },
-          {
-            source: "/projects/dimmmensions/:path*",
-            destination:
-              "https://marcbouchenoire-dimmmensions.vercel.app/:path*"
-          }
-        ]
-      }
+      return [
+        {
+          source: "/:path*",
+          destination: `/:path*`
+        },
+        {
+          source: "/projects/symbolist/:path*",
+          destination: "https://marcbouchenoire-symbolist.vercel.app/:path*"
+        },
+        {
+          source: "/projects/typometer/:path*",
+          destination: "https://marcbouchenoire-typometer.vercel.app/:path*"
+        },
+        {
+          source: "/projects/dimmmensions/:path*",
+          destination: "https://marcbouchenoire-dimmmensions.vercel.app/:path*"
+        }
+      ]
     }
   }
 }
