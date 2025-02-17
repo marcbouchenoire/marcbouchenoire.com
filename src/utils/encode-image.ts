307244cb15eff62ea40b3ce056f30ed64415d0a9
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-02-17 12:02:30 +0100

    Simplify activity SVG endpoints

--- src/utils/encode-image.ts
+++ src/utils/encode-image.ts
@@ -1,4 +1,7 @@
+"use cache"
+
 import { toBase64 } from "fast-base64/js"
+import { unstable_cacheLife as cacheLife } from "next/cache"
 
 /**
  * Fetch an image and encode it as a base64 data URI.
@@ -6,6 +9,8 @@ import { toBase64 } from "fast-base64/js"
  * @param url - The image's URL.
  */
 export async function encodeImage(url: string) {
+  cacheLife("max")
+
   const response = await fetch(url)
   const buffer = await response.arrayBuffer()
   const array = new Uint8Array(buffer)
